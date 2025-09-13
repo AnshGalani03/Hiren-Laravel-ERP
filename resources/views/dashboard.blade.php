63d6062f4d25b171429adfa0cea7a083c79046ab
AnshGalani03 <anshgalani003@gmail.com>
2025-09-13 10:35:13 +0530

    Update dashboard project card only show active project count number

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -58,7 +58,7 @@
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-uppercase mb-1">
                                         Active Projects</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Models\Project::count() }}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ \App\Models\Project::where('active', true)->count() }}</div>
                                 </div>
                                 <div class="col-auto">
                                     <i class="fas fa-project-diagram fa-2x text-gray-300"></i>
